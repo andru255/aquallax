57e7d665334c93824e78c15810e05dca02c8f385
Andrés Muñoz <andru255@gmail.com>
2018-03-12 00:31:04 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require("gulp");
+var postCSS                = require("gulp-postcss");
+var importPostCSS          = require("postcss-import");
+var mixinsPostCSS     = require("postcss-mixins");
+var nestedPostCSS     = require("postcss-nested");
+var simpleVarsPostCSS = require("postcss-simple-vars");
+var resetPostCSS      = require("postcss-autoreset");
+var gradientsPostCSS  = require("postcss-easing-gradients");
+var autoprefixer      = require("autoprefixer");
+var babel             = require("gulp-babel");
+var concat            = require("gulp-concat");
+var uglify            = require("uglify-js");
+var composer          = require("gulp-uglify/composer");
+var minify            = composer(uglify, console);
+var pump              = require("pump");
+var del               = require("del");
+var runSequence       = require("run-sequence");
+var browserSync       = require("browser-sync").create();
+var concatCSS         = require("gulp-concat-css");
+
+gulp.task('clean', function(){
+    return del([ 
+        './build/*.*' 
+    ]);
+});
+
+gulp.task('css', function(){
+    return gulp.src([ './src/styles/*.css', './src/styles/**/*.css' ])
+        .pipe(postCSS([
+            importPostCSS(),
+            mixinsPostCSS(),
+            simpleVarsPostCSS(),
+            nestedPostCSS(),
+            autoprefixer(),
+            gradientsPostCSS(),
+            resetPostCSS()
+        ]))
+        .pipe(concatCSS("main.css"))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('assets', function(){
+    return gulp.src(`./src/assets/*.*`)
+        .pipe(gulp.dest('./build/assets'));
+});
+
+gulp.task('html', function(){
+    return gulp.src('./src/example/*.html')
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('js', function(){
+    return gulp.src([ 
+            './src/scripts/**/*.js', 
+            './src/scripts/*.js' 
+        ]).pipe(babel({
+            presets: ["env"],
+            plugins: ["transform-remove-strict-mode"]
+        }))
+        .pipe(concat('min.js'))
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('js-min', function(cb){
+    var options = {
+        mangle: {
+            toplevel: true
+        },
+        ie8: true
+    };
+    return pump([
+        gulp.src("./build/min.js"),
+        minify(options),
+        gulp.dest("./build")
+    ]);
+});
+
+//development
+gulp.task('server', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            directory: true
+        }
+    });
+
+    gulp.watch([ "./src/styles/*.*", "./src/styles/**/*.*"], ['css']);
+    gulp.watch([ "./src/scripts/*.*", './src/scripts/**/*.js'], ['js']);
+    gulp.watch("./src/example/index.html", ['html']);
+    gulp.watch("./src/assets/*.*", ['assets']);
+
+    gulp.watch("./build/*.css").on("change", browserSync.reload);
+    gulp.watch("./build/*.html").on("change", browserSync.reload);
+    gulp.watch("./build/*.js").on("change", browserSync.reload);
+});
+
+gulp.task("default", function(cb){
+    return runSequence("clean", 
+                       ["assets", "html","css", "js"], 
+                       "js-min", cb);
+});
---
+++ src/scripts/main.js
@@ -0,0 +1,55 @@
+//b-slide--article-banner
+var sticky = document.getElementsByClassName("b-sticky")[0];
+var splash = document.getElementsByClassName("b-splash")[0];
+var mainContainer = document.getElementsByClassName("b-main-container")[0];
+
+/*utils*/
+function getWindowSize() {
+    var width = window.innerWidth
+        || window.documentElement.clientWidth
+        || document.body.clientWidth;
+    var height = window.innerHeight
+        || window.documentElement.clientHeight
+        || document.body.clientHeight;
+
+    return {
+        "width": width,
+        "height": height
+    };
+}
+
+/*gets the window height and resize the first banner*/
+function applyStylesToElement(target, rules) {
+    var strRules = "";
+    for (var rule in rules) {
+        strRules += [rule, ":", rules[rule], ";"].join("");
+    }
+    target.setAttribute("style", strRules);
+}
+
+function resizeSplashAndMainBlock(){
+    applyStylesToElement(splash, {
+        "height": getWindowSize().height + "px"
+    });
+    applyStylesToElement(mainContainer, {
+        "margin-top": getWindowSize().height + "px"
+    });
+};
+resizeSplashAndMainBlock();
+
+window.addEventListener("resize", function(){
+    resizeSplashAndMainBlock();
+});
+
+/*sticky header*/
+var stickyDefaultClass = sticky.className;
+var stickyFixedClass = [ stickyDefaultClass, "--fixed" ].join("");
+window.addEventListener("scroll", function(){
+    var mainTopPosition = mainContainer.getBoundingClientRect().top;
+    if (mainTopPosition < 0) {
+        sticky.className = [stickyDefaultClass, stickyFixedClass].join(" ");
+    } else {
+        sticky.className = stickyDefaultClass;
+    }
+});
+

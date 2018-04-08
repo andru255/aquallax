6d741397f139baa8d02fe26e9c296913088828f0
Andrés Muñoz <andru255@gmail.com>
2018-04-08 11:53:27 -0500

    feat(transitions): implementing for smooth css changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 //b-slide--article-banner
 var sticky = document.getElementsByClassName("b-sticky")[0];
 var splash = document.getElementsByClassName("b-splash")[0];
+var splashInner = document.getElementsByClassName("b-splash__inner")[0];
 var mainContainer = document.getElementsByClassName("b-main-container")[0];
 
 /*utils*/
@@ -42,14 +43,37 @@ window.addEventListener("resize", function(){
 });
 
 /*sticky header*/
+
+/* switching class */
 var stickyDefaultClass = sticky.className;
 var stickyFixedClass = [ stickyDefaultClass, "--fixed" ].join("");
-window.addEventListener("scroll", function(){
+function classNameSwitchStickyBar(){
     var mainTopPosition = mainContainer.getBoundingClientRect().top;
     if (mainTopPosition < 0) {
         sticky.className = [stickyDefaultClass, stickyFixedClass].join(" ");
     } else {
         sticky.className = stickyDefaultClass;
     }
-});
+}
+/* splash fadeout onscroll animation */
+function fadeOutSplashContent(){
+    var mainTopPosition = mainContainer.getBoundingClientRect().top;
+    var windowHeight = getWindowSize().height;
+    var visible = windowHeight - (windowHeight - mainTopPosition);
+    var percent = ( visible * 100 ) / windowHeight;
+    var opacity =  percent / 100;
 
+    console.log("percent", percent);
+    console.log("opacity", opacity);
+
+    applyStylesToElement(splashInner, {
+        "opacity": opacity,
+        "-webkit-transform": percent > 80 ? "translate3d(0, " + percent + "px, 0)": ""
+    });
+    //"-webkit-transform": "translate3d(0, " + percent +"px, 0)"
+}
+
+window.addEventListener("scroll", function(){
+    classNameSwitchStickyBar();
+    fadeOutSplashContent();
+});
